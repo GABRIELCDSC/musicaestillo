f2e220c9172e5680fab6fe0bbfe9399c6ed8572d
Camargo <gabrielcarvalhocamargo007@gmail.com>
2024-10-07 07:51:43 -0300

    first commit

---
+++ playlist/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Playlist, Musica
+
+class PlaylistForm(forms.ModelForm):
+        class Meta:
+            model = Playlist
+            fields = '__all__'
+
+class MusicaForm(forms.ModelForm):
+      class Meta:
+            model = Musica
+            fields = '__all__'
+    
